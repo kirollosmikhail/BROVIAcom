5f57d0c12e6da8dc57ae46d5dcab8314471430f6
Giorgia Ronzitti <giorgia.ronzitti17@gmail.com>
2024-02-13 11:21:01 +0100

    classe Tipi_Commesse

---
+++ BROVIAcom/App_Code/TIPI_COMMESSE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class TIPI_COMMESSE
+{
+    public int Cod_Tipo_Commessa;
+    public string Nome_Commessa;
+
+    public TIPI_COMMESSE()
+    {
+        
+    }
+}
