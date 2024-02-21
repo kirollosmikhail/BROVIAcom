4b062cbc85e3b02bbd71a42f3a2e0e428055fdff
Truffaud13 <marzagalligiacomo@gmail.com>
2024-02-21 09:54:08 +0100

    BUG Zoom sistemato

--- BROVIAcom/JS/AdattaTabella.js
+++ BROVIAcom/JS/AdattaTabella.js
@@ -1,24 +1,60 @@
 ﻿// Funzione per calcolare la larghezza della finestra del browser
-    function getWindowWidth() {
-        return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    }
+function getWindowWidth() {
+    return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+}
 
-    // Funzione per verificare se la larghezza della finestra è inferiore alla larghezza di .table-container
-    function checkWidth() {
+// Funzione per verificare se la larghezza della finestra è inferiore alla larghezza di .table-container
+function checkWidth() {
+    if (document.querySelector('.table-container') != null) {
+        var tableContainer = document.querySelector('.table-container');
         var table = document.querySelector('.table-container').querySelector('table');
-    var windowWidth = getWindowWidth();
+        var containerWidth = document.querySelector('.container');
 
-    if (windowWidth < table.offsetWidth) {
-        // La larghezza della finestra è inferiore alla larghezza di .table-container
-        // Rimuovi la classe overflow-auto se presente
-        tableContainer.classList.remove('overflow-auto');
+        if (containerWidth.offsetWidth > table.offsetWidth) {
+            // La larghezza della finestra è inferiore alla larghezza di .table-container
+            // Rimuovi la classe overflow-auto se presente
+            tableContainer.classList.remove('overflow-auto');
+            console.log("Ciao, mondo!")
         } else {
-        // La larghezza della finestra è maggiore o uguale alla larghezza di .table-container
-        // Aggiungi la classe overflow-auto
-        tableContainer.classList.add('overflow-auto');
+            // La larghezza della finestra è maggiore o uguale alla larghezza di .table-container
+            // Aggiungi la classe overflow-auto
+            tableContainer.classList.add('overflow-auto');
         }
     }
+    var width = window.screen.width;
+    if (width <= 600) {
+        // Se la larghezza della finestra è inferiore o uguale a 600px, imposta lo zoom a 1.9
+        document.body.style.zoom = 1.9;
+    }
+    else if (width <= 990) {
+        // Se la larghezza della finestra è inferiore o uguale a 990px, imposta lo zoom a 1.4
+        document.body.style.zoom = 1.4;
+    }
+    else {
+        // Altrimenti, lascia lo zoom predefinito
+        document.body.style.zoom = 1;
+    }
+}
 
-    // Esegui la funzione all'avvio della pagina e ogni volta che la finestra viene ridimensionata
-    window.onload = checkWidth;
-    window.onresize = checkWidth;
+function checkWidth2() {
+    if (document.querySelector('.table-container') != null) {
+        var tableContainer = document.querySelector('.table-container');
+        var table = document.querySelector('.table-container').querySelector('table');
+        var containerWidth = document.querySelector('.container');
+
+        if (containerWidth.offsetWidth > table.offsetWidth) {
+            // La larghezza della finestra è inferiore alla larghezza di .table-container
+            // Rimuovi la classe overflow-auto se presente
+            tableContainer.classList.remove('overflow-auto');
+            console.log("Ciao, mondo!")
+        } else {
+            // La larghezza della finestra è maggiore o uguale alla larghezza di .table-container
+            // Aggiungi la classe overflow-auto
+            tableContainer.classList.add('overflow-auto');
+        }
+    }
+}
+// Esegui la funzione all'avvio della pagina e ogni volta che la finestra viene ridimensionata
+window.onload = checkWidth;
+
+window.onresize = checkWidth2;
--- BROVIAcom/login/login.aspx
+++ BROVIAcom/login/login.aspx
@@ -10,7 +10,7 @@
     <link href="../css/StyleSheet.css" rel="stylesheet" />  
     <link rel="icon" type="image/x-icon" href="../img/BROVIAico.png" />
 </head>
-<body style="zoom:1.4;">
+<body >
     <form id="form1" runat="server">
         <div class="login-container">
             <img src="../img/BROVIAcom.png" />
@@ -22,5 +22,6 @@
         </div>
     </form>
 </body>
+<script src="../JS/AdattaTabella.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
